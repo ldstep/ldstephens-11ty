835b74cd7b205183b6ef2e4976e035d45c76d18b
Loren Stephens <ldstep@ldstephens.com>
2025-02-17 15:23:15 -0500

    getting started

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+   return {
+      dir: {
+         input: "src", // Your source files
+         output: "public", // Ensure Eleventy outputs here
+      },
+   };
+};
